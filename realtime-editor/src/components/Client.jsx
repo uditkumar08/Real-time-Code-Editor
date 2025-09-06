8186f77323340b4303ec9caec0307c281a1d4ac9
uditkumar08 <udit2162@gmail.com>
2025-09-06 23:53:59 +0530

    some

--- realtime-editor/src/components/Client.jsx
+++ realtime-editor/src/components/Client.jsx
@@ -4,7 +4,7 @@ export const Client = ({username}) => {
   return (
     <div className='client'>
         <Avatar name={username} size={50} round="14px"/>
-        <span className='userName'>{username}</span>
+        <span className='userName'>{username}</span>    
     </div>
   )
 }
